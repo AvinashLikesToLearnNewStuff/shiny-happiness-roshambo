d6cda0fd80427033f786625820b6cf5af1c47c7f
AvinashProjects <106229044+AvinashLikesToLearnNewStuff@users.noreply.github.com>
2023-05-23 17:55:57 +0530

    added who won function

--- script.js
+++ script.js
@@ -9,33 +9,49 @@ function getComputerChoice(){
 function playRound(playerSelection, ComputerSelection){
     playerSelection = playerSelection.toLowerCase();  // converting the player string to lower case
     let computerSelection = getComputerChoice();
-    console.log("player " + playerSelection + " computer " + computerSelection);
+//    console.log("player " + playerSelection + " computer " + computerSelection); //this is here for debugging 
 
     if ( (playerSelection == 'rock' && computerSelection == "scizzor")
 	 ||  (playerSelection == 'paper' && computerSelection== "rock") ||
 	 (playerSelection == 'scizzor' && computerSelection =="paper")
-       ) {return `You win! ${playerSelection} beats ${computerSelection}`}
+       ) {console.log( `You win! ${playerSelection} beats ${computerSelection}`);return 1;}
 
 
     else if (
 	(playerSelection == "rock" && computerSelection == "paper")
 	    ||  (playerSelection == "paper" && computerSelection== "scizzor") ||
 	    (playerSelection == "scizzor" && computerSelection =="rock")
-    ) {return `you lose! ${computerSelection} beats ${playerSelection}`}
+    ) {console.log( `you lose! ${computerSelection} beats ${playerSelection}`); return -1;}
 
     else {
-	return `it's a tie, you chose ${playerSelection} and the computer chose ${computerSelection} `}
+	console.log( `it's a tie, you chose ${playerSelection} and the computer chose ${computerSelection} `);return 0;}
 }
 
 function play5Rounds(){
-let playerSelection;
-let computerSelection = getComputerChoice();
+    let playerSelection;
+    let playerScore=0;
+    let computerScore=0;
+    let computerSelection = getComputerChoice();
     for (let i = 0; i < 5; i++){
-    console.log(`Round ${i+1}`);
-    computerSelection = getComputerChoice();
-    playerSelection = prompt("enter rock,paper or scizzor");
-        console.log(playRound(playerSelection, computerSelection));
+	console.log(`Round ${i+1}`);
+	computerSelection = getComputerChoice();
+	playerSelection = prompt("enter rock,paper or scizzor");
+
+	let scoreOfThisRound = playRound(playerSelection, computerSelection);
+
+	if (scoreOfThisRound == 1){playerScore++;}
+	else if (scoreOfThisRound==-1){computerScore++;}
+	else{}
+	console.log(`playerScore = ${playerScore} and computerScore = ${computerScore}`);   
+
     }
+    whoWon(playerScore, computerScore);
+}
+
+function whoWon(playerScore,computerScore){
+     if(playerScore>computerScore) {console.log("player Wins!!");}
+      else if(playerScore<computerScore) {console.log("computer Wins");}
+      else {console.log("its a tie");}
 }
 
 play5Rounds();
